2c1f96fe11b654d5b269a64cebcc397f704efa97
dawsonren <dawsonren@gmail.com>
2025-12-05 15:04:39 -1000

    final

--- install_packages.R
+++ install_packages.R
@@ -1,2 +1,2 @@
 # Install required packages for the analysis
-install.packages(c("tidyverse", "glmnet", "caret", "corrplot", "rmarkdown"), repos = "http://cran.us.r-project.org")
+install.packages(c("tidyverse", "glmnet", "caret", "corrplot", "rmarkdown", "pROC", "rpart", "rpart.plot", "e1071"), repos = "http://cran.us.r-project.org")
